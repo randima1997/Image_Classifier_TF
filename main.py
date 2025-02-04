12aef1864af2151134a23d47bdf261b8ee284a60
Randima Fernando <randimafernando1997@gmail.com>
2025-02-04 18:02:28 +0100

    Set up the libraries and included the gitignore

---
+++ main.py
@@ -0,0 +1,12 @@
+import keras.datasets
+import keras.datasets.mnist
+import keras.losses
+import keras.metrics
+import keras.optimizers
+import keras.utils
+import tensorflow as tf
+import keras
+from keras import layers
+
+
+
